9865151823b341b96b7eb31a7ba279a9d87a93ce
Manuel <WtfJoke@users.noreply.github.com>
2022-06-12 13:31:31 +0200

    feat: Add python publishing (#6)
    * chore: fix readme
    
    * feat: publish python construct

--- .projenrc.js
+++ .projenrc.js
@@ -1,23 +1,29 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'WtfJoke',
-  authorAddress: 'wtfjoke@livenet.ch',
-  cdkVersion: '2.25.0',
-  defaultReleaseBranch: 'main',
-  name: 'cdk-github',
-  repositoryUrl: 'https://github.com/wtfjoke/cdk-github.git',
-  license: 'MIT',
+  authorName: 'Manuel',
+  authorAddress: 'https://github.com/wtfjoke/',
   copyrightOwner: 'Manuel <WtfJoke>',
   copyrightPeriod: '2022',
+  license: 'MIT',
+  repositoryUrl: 'https://github.com/WtfJoke/cdk-github',
+  description: "AWS CDK Construct Library to interact with GitHub's API.",
   keywords: ['cdk', 'github', 'constructs', 'aws'],
+
+  name: 'cdk-github',
+  cdkVersion: '2.25.0',
+  defaultReleaseBranch: 'main',
   deps: ['@octokit/core', 'libsodium', 'libsodium-wrappers', '@aws-sdk/client-secrets-manager'], /* Runtime dependencies of this module. */
   bundledDeps: ['@octokit/core', 'libsodium', 'libsodium-wrappers', '@aws-sdk/client-secrets-manager'], /* Dependencies that are bundled with this module. */
-  description: 'A CDK library for GitHub',
   devDeps: ['esbuild', '@types/libsodium-wrappers', '@types/aws-lambda', 'nock', 'aws-sdk-client-mock'], /* Build dependencies for this module. */
   gitignore: ['cdk.out'],
   scripts: {
     'cdk:actionsecret:deploy': 'npx cdk deploy --app "npx ts-node --prefer-ts-exts src/examples/action-secret/action-secret-app.ts"',
     'cdk:actionenvironmentsecret:deploy': 'npx cdk deploy --app "npx ts-node --prefer-ts-exts src/examples/action-environment-secret/action-environment-secret-app.ts"',
+  },
+  publishToPypi: {
+    distName: 'cdk-github',
+    module: 'cdk-github',
   },
   codeCov: true,
 });
